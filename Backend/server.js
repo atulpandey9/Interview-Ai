7464e315e4064a51999f891d1b4f59f74a07e854
AP <atul3pandey5@gmail.com>
2026-07-12 22:39:33 +0530

    production changes

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,14 @@
 require('dotenv').config();
 const app = require("./src/app");
 const connectToDB = require("./src/config/database");
-const PORT = process.env.PORT || 5000 ;
+const port= process.env.PORT || 5000 ;
 connectToDB();
 
 app.get('/',(req,res)=>{
   return res.send("server is running")
 })
 
-app.listen(PORT, () => {
-  console.log(`server is running in port ${PORT}`);
+app.listen(port, () => {
+  console.log(`server is running in port ${port}`);
 });
 
